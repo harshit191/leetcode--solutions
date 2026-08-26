fc35f201553ba9efb8f5fc70fd898306ef85dd42
harshit191 <mittalharshit331@gmail.com>
2026-08-26 13:32:12 +0530

    Time: 0 ms (100%), Space: 8 MB (74.83%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0;
+        int high = 0;
+
+        for (char ch : s) {
+
+            if (ch == '(') {
+                low++;
+                high++;
+            }
+            else if (ch == ')') {
+                low--;
+                high--;
+            }
+            else { // '*'
+                low--;   // '*' ko ')' maan liya
+                high++;  // '*' ko '(' maan liya
+            }
+
+            if (high < 0)
+                return false;
+
+            if (low < 0)
+                low = 0;
+        }
+
+        return low == 0;
+    }
+};
