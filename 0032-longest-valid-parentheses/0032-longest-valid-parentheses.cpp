0120117943f9d81beccb7153d6aea7195568dd98
harshit191 <mittalharshit331@gmail.com>
2026-08-26 14:09:42 +0530

    Time: 0 ms (100%), Space: 11.7 MB (50.36%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+
+        stack<int> st;
+        st.push(-1);
+
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+
+                if(st.empty())
+                {
+                    st.push(i);
+                }
+                else
+                {
+                    int len = i - st.top();
+
+                    if(len > ans)
+                        ans = len;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
